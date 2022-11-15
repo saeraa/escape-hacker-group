b8641fb6b6176808f3d6aa7841ec01ee040e7952
Sara <saeraa@gmail.com>
2022-11-15 10:56:58 +0100

    dummy data

---
+++ 02-esc/static/dummy-data.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		title: "room one",
+		description: "room one is exciting",
+		type: "online",
+		min_participants: "2",
+		max_participants: "3",
+		rating: "4",
+		image: "",
+		tags: ["exciting", "creative", "intense"]
+	},
+	{
+		title: "room two",
+		description: "room two is fabulous",
+		type: "on-site",
+		min_participants: "4",
+		max_participants: "6",
+		rating: "4",
+		image: "",
+		tags: ["exciting", "creative", "fabulous"]
+	},
+	{
+		title: "room three",
+		description: "room three is super scary",
+		type: "on-site",
+		min_participants: "2",
+		max_participants: "5",
+		rating: "3",
+		image: "",
+		tags: ["exciting", "scary", "intense"]
+	}
+];

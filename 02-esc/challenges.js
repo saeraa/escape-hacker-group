a43a7ea07fbabde434fad72cda6af3eb4152afdc
DanneCarlson <113188007+DanneCarlson@users.noreply.github.com>
2022-11-16 15:02:08 +0100

    Adding JS

---
+++ 02-esc/challenges.js
@@ -0,0 +1,21 @@
+/* Objekt ska skapas med värden som kommer via API*/
+
+const challengeCard = {
+    title: "" ,
+    description: "" ,
+    type: "" ,
+    minParticipants: "" ,
+    maxParticipants: "" ,
+    rating: "" ,
+    url: "" ,
+    label: ""
+
+
+};
+
+
+function createChallengeCard () {
+    // Skapa objektet challengeCard från API
+
+
+};

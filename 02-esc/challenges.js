3e11bbb4457ebd8c5c9cd47dee486930e8d49b5b
DanneCarlson <113188007+DanneCarlson@users.noreply.github.com>
2022-11-18 14:20:14 +0100

    Minor updates

--- 02-esc/challenges.js
+++ 02-esc/challenges.js
@@ -1,22 +1,11 @@
 
-// Strunta i att göra objekt, göra en array istället?
-/*const challengeCard = {
-    title: "" ,
-    description: "" ,
-    type: "" ,
-    minParticipants: "" ,
-    maxParticipants: "" ,
-    rating: "" ,
-    urlPicture: "" ,
-    labelList: ""
-};*/
-
 // Testknapp för att kolla att data laddas från API
 const testBtn = document.querySelector(".testBtn");
 testBtn.addEventListener("click", getChallengesAPI);
 
 // Array för att spara ned data från API
 let resultFromAPI = []; 
+
 
     async function getChallengesAPI() {
         const resultList = await fetch(
@@ -25,17 +14,33 @@ let resultFromAPI = [];
         const result = await resultList.json();
         result.challenges.forEach((challenge) => {
             resultFromAPI.push(challenge);
-            //console.log(challenge.rating);   
+            //console.log(challenge.description);   
+            //console.log(resultFromAPI);
             });
 
-            // Anropa funktion härifrån för att bygga upp listan och visa på challenges.html?
-            // Typ showAllChallenges()
+            // Anropar funktion för att bygga upp listan och visa på challenges.html
+            showAllChallenges(resultFromAPI);
         };
     
 
-    function showAllChallenges () {
+    function showAllChallenges (resultFromAPI) {
+        //console.log(resultFromAPI[2].title);
 
         let challenges = document.getElementById('challenge-list')
+        let testVisning = document.getElementById('.challenge-title')
 
-        // Appenda till listan här
+        for (i = 0; i < resultFromAPI.length; i++) {
+            let title = resultFromAPI[i].title;
+            console.log(title);
+            
+            
+            // Enligt nedan kan en ju inte hålla på - tips?
+            /*
+            let card = document.createElement("li");
+            let id = document.createElement("span");
+            o.s.v. för att sen appenda
+            */
+            
+            
+        }    
     };

dfe68d3ff15bcb0f55ab611a4ffbb94a80e7d914
jakobpalmborg <jakob.palmborg@gmail.com>
2022-11-22 14:06:06 +0100

    delete html placeholder file and changed varible declaration and eventlistener

--- 02-esc/modal.js
+++ 02-esc/modal.js
@@ -1,22 +1,27 @@
-const openModalBtn = document.querySelector('.open-modal-btn').addEventListener("click", openModal);
+const openModalBtn = document.querySelector('.open-modal-btn');
+openModalBtn.addEventListener("click", openModal);
+
 
 function openModal() {
     let body = document.querySelector('body');
     let modalDiv = document.createElement('div');
     modalDiv.className = 'modal-div';
     modalDiv.innerHTML = `
+        <div class="bookingStep1Content">
         <h1>Book room "Title of room" (step 1)</h1>
         <h2>What date would you like to come?</h2>
         <form action="">
             <label class="booking-date-label" for="date">Date</label>
             <input class="input-field" type="date" name="date">
-            <input type="submit" value="Search available times" class="button primary open-modal-step-2">
+            <input id="firstButton" type="submit" value="Search available times" class="button primary open-modal-step-2">
         </form>
+        </div>
     `;
     body.appendChild(modalDiv);
 
 
-    let openModalStepTwoBtn = document.querySelector(".open-modal-step-2").addEventListener("click", 
+    let openModalStepTwoBtn = document.querySelector(".open-modal-step-2");
+    openModalStepTwoBtn.addEventListener("click", 
     openModalStepTwo);
 
     function openModalStepTwo(e) {
@@ -25,6 +30,7 @@ function openModal() {
         console.log('modal two opened');
 
         modalDiv.innerHTML = `
+            <div class="bookingStep2Content">
             <h1>Book room "Title of room" (step 2)</h1>
             <form action="">
                 <label for="name">Name</label>
@@ -43,9 +49,11 @@ function openModal() {
                 </select>
                 <input class="button primary open-modal-step-3" type="submit" value="Submit booking">
             </form>
+            </div>
         `;
     
-    let openModalStepThreeBtn = document.querySelector(".open-modal-step-3").addEventListener("click", 
+    let openModalStepThreeBtn = document.querySelector(".open-modal-step-3");
+    openModalStepThreeBtn.addEventListener("click", 
     openModalStepThree);
 
     function openModalStepThree(e) {
@@ -54,10 +62,13 @@ function openModal() {
         console.log('modal three opened');
 
         modalDiv.innerHTML = `
+        <div class="bookingStep3Content">
             <h1>Thank you!</h1>
             <a href="">Back to challenges</a>
+         </div>
         `;
 
     }
 }
 }
+

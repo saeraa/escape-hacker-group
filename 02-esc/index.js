de7d6680cf1947a28fec2c6f40f59bf0c795c348
Patrik Sjöstrand <patrik.sjostrand@gmail.com>
2022-11-29 16:07:52 +0100

    add function and for-loop

--- 02-esc/index.js
+++ 02-esc/index.js
@@ -1,4 +1,48 @@
 import { getDataFromAPI } from "./getDataFromAPI.js";
 
+
 // getDataFromAPI returns an array with all the API data
 // console.log(await getDataFromAPI());
+
+
+/* Testing in challenge.html site first */
+
+
+
+ let resFromAPI = []; 
+ let ratings = [];
+
+
+// First get all API data from getDataFromAPI
+
+ resFromAPI = await getDataFromAPI(); 
+
+ //console.log(getDataFromAPI.rating);
+
+
+// Then select the three highest rating-star with an if statements
+
+  function getThreeHighestRatingStar(resFromAPI){
+
+  for(let i = 0; i < resFromAPI.length; i++){
+
+     ratings = resFromAPI[i].rating;
+     console.log(ratings);
+  }
+
+  
+
+
+}  
+
+getThreeHighestRatingStar(resFromAPI);
+
+
+
+/* setThreeHighestRate(); */
+
+
+
+
+
+// and then show them in site index.html.

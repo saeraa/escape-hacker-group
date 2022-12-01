435589c5240b0bafe5077ed4fceacbeec5ba2073
Sara <saeraa@gmail.com>
2022-12-01 10:51:38 +0100

    bundling

---
+++ 02-esc/index.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.querySelector(".main-nav-toggle").addEventListener("click",(()=>{document.querySelector(".main-nav").classList.toggle("open")})),document.querySelector("h1").addEventListener("click",(()=>{window.location.href="index.html"}))})();
---
+++ 02-esc/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: {
+		index: "./index.js",
+		challenge: "./challenges.js"
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname)
+	}
+};

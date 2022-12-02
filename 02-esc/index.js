04f96b4233dc39e436f844c64966b6ed91dc6888
Patrik Sjöstrand <patrik.sjostrand@gmail.com>
2022-12-02 11:11:32 +0100

    add three highest value

--- 02-esc/index.js
+++ 02-esc/index.js
@@ -1,9 +1,11 @@
-/* import { info } from "sass"; */
+/* import { info } from "sass";  */
 import { getDataFromAPI } from "./getDataFromAPI.js";
+
+const challenge_list = document.querySelector('.challenge-list');
 
 
 // getDataFromAPI returns an array with all the API data
-// console.log(await getDataFromAPI());
+//console.log(await getDataFromAPI());
 
 
 /* Testing in challenge.html site first */
@@ -16,13 +18,39 @@ import { getDataFromAPI } from "./getDataFromAPI.js";
 
 // First get all API data from getDataFromAPI
 
- resFromAPI = await getDataFromAPI(); 
+resFromAPI = await getDataFromAPI();
 
  //console.log(getDataFromAPI.rating);
 
  //ratings = resFromAPI.rating;
 
  //console.log(resFromAPI[0].rating);
+
+
+ /* control function */
+
+function checkRatingIndex(resFromAPI){
+
+  let r = [];
+
+  for(let i = 0; i < resFromAPI.length; i++){
+
+    r = resFromAPI[i].rating;
+
+
+    if(r === 4.5){
+      return r;
+    }
+  }
+}
+
+console.log(resFromAPI.findIndex(checkRatingIndex));
+ 
+
+
+ 
+
+
 
 
 
@@ -36,17 +64,21 @@ import { getDataFromAPI } from "./getDataFromAPI.js";
     let secondHighest = -Infinity;
     let thirdHighest = -Infinity;
     let r;
+    
+    
 
     // get the first highest rating-star
 
     for(let n = 0; n < resFromAPI.length; n++){
 
        r = resFromAPI[n].rating;
+       
 
       highest = Math.max(highest, r);
       
     }
     console.log(highest);
+    
 
 
     // get the second-highest rating-star  
@@ -55,10 +87,12 @@ import { getDataFromAPI } from "./getDataFromAPI.js";
     for(let n = 0; n < resFromAPI.length; n++){
 
       r = resFromAPI[n].rating;
+      
 
       if(r < highest){        
   
         secondHighest = Math.max(secondHighest, r);
+        
       }
     }
     console.log(secondHighest);
@@ -79,13 +113,21 @@ import { getDataFromAPI } from "./getDataFromAPI.js";
     console.log(thirdHighest);
 
 
-
+  /************** creating of card ***************/
     
   
 
   for(let i = 0; i < resFromAPI.length; i++){
 
-    ratings = resFromAPI[i].rating;
+    //ratings = resFromAPI[i].rating;
+
+    let id = resFromAPI[i].id;
+    let type = resFromAPI[i].type;
+    let title = resFromAPI[i].title;
+    let description = resFromAPI[i].description;
+    let minParticipants = resFromAPI[i].mindescription;
+    let maxParticipants = resFromAPI[i].maxParticipants;
+    let image = resFromAPI[i].image;
 
     
     
@@ -94,6 +136,24 @@ import { getDataFromAPI } from "./getDataFromAPI.js";
 
     let challenge_item = document.createElement('li');
     challenge_item.classList.add('challenge-item');
+
+    let setImage = document.createElement('img');
+    setImage.classList.add('challenge-image');
+    setImage.src = image;
+    challenge_item.append(setImage);
+
+    let setRoomImage = document.createElement('image');
+    setRoomImage.classList.add('room-image');
+      if(type == 'onsite')
+      {
+        setRoomImage.src = "static/iconOnline.svg"
+      }
+      else
+      {
+        setRoomImage.src = "static/iconicOnsite.svg";
+      }
+
+    challenge_item.append(setRoomImage);
 
 
     // create dynamic ul 
@@ -123,47 +183,78 @@ import { getDataFromAPI } from "./getDataFromAPI.js";
     setRatingStar5.classList.add('rating-star');
     setRatingUl.appendChild(setRatingStar5);
 
-    if(ratings == 2 || ratings < 3){
+    if(r == highest){
 
       // print out between 2 < 3
       setRatingStar1.classList.add('active');
       setRatingStar2.classList.add('active');
+      setRatingStar3.classList.add('active');
+      setRatingStar4.classList.add('active');
     }
 
 
-    else if(ratings == 3 || ratings < 4 ){
+    else if(r < secondHighest){
 
       // Print out between 3 < 4
       setRatingStar1.classList.add('active');
       setRatingStar2.classList.add('active');
       setRatingStar3.classList.add('active');
+      setRatingStar4.classList.add('active');
       
     }
-    else if(ratings == 4 || ratings < 5){
+    else if(r < thirdHighest){
 
       // print out between 4 < 5
       setRatingStar1.classList.add('active');
       setRatingStar2.classList.add('active');
       setRatingStar3.classList.add('active');
-      setRatingStar4.classList.add('active');
+      
     }
-    else if(ratings == 5){
-
-      // print out 5
-      setRatingStar1.classList.add('active');
-      setRatingStar2.classList.add('active');
-      setRatingStar3.classList.add('active');
-      setRatingStar4.classList.add('active');
-      setRatingStar5.classList.add('active');
     
-    }
 
-    // adding ul to li in card
+   
 
     challenge_item.appendChild(setRatingUl);
 
-     
-     //console.log(ratings);
+    let set_Title = document.createElement('h3');
+    set_Title.classList.add('challenge-title');
+    set_Title.textContent = title;
+    challenge_item.appendChild(set_Title);
+
+    let set_Participants = document.createElement('small');
+    set_Participants.classList.add('challenge-meta');
+    set_Participants.innerHTML = `${minParticipants} - ${maxParticipants} participants`;
+    challenge_item.appendChild(set_Participants);
+
+    let set_Description = document.createElement('p');
+    set_Description.classList.add('challenge-description');
+    set_Description.textContent = description;
+    challenge_item.appendChild(set_Description);
+
+    let btn_Book = document.createElement('button');
+    btn_Book.classList.add('btnBook');
+    btn_Book.setAttribute('data-id', id);
+
+      if(type == 'onsite')
+      {
+        btn_Book.textContent = 'Take challenge online';
+      }else
+      {
+        btn_Book.textContent = 'Book this room';
+      }
+
+    challenge_item.appendChild(btn_Book);
+
+    btn_Book.addEventListener('click', function(){
+
+      // referens till bokningsfunktionen
+    });
+
+    challenge_list.appendChild(challenge_item);
+
+
+
+   
   }
 
   
@@ -177,10 +268,10 @@ getThreeHighestRatingStar(resFromAPI);
 
 
 
-/* setThreeHighestRate(); */
 
 
 
 
 
-// and then show them in site index.html.
+
+

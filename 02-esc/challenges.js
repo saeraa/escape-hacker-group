40ab2dab9ce2022cd7c70dd9865682fe473cacc3
Patrik Sjöstrand <patrik.sjostrand@gmail.com>
2022-12-03 10:59:17 +0100

    add function for three cards

--- 02-esc/challenges.js
+++ 02-esc/challenges.js
@@ -1,5 +1,6 @@
 
 import { getDataFromAPI } from "./getDataFromAPI.js";
+import { showAllChallenges } from "./showAllChallenges.js";
 const testBtn = document.querySelector(".testBtn");
 //testBtn.addEventListener("click", getChallengesAPI);
 const challenge_list = document.querySelector('.challenge-list');
@@ -12,132 +13,8 @@ let resultFromAPI = [];
 
 window.onload = getChallengesAPI();
 
-    async function getChallengesAPI() {
-       resultFromAPI = await getDataFromAPI();
+async function getChallengesAPI() {
+    resultFromAPI = await getDataFromAPI();
 
-            showAllChallenges(resultFromAPI);
-           //  challenges_visiility.style.display = 'block'; 
-        };
-    
-    function showAllChallenges (resultFromAPI) {
-    
-        for (let i = 0; i < resultFromAPI.length; i++) {
-            let id = resultFromAPI[i].id;
-            let type = resultFromAPI[i].type;
-            let rating = resultFromAPI[i].rating;
-            let title = resultFromAPI[i].title; 
-            let description = resultFromAPI[i].description;
-            let minParticipants = resultFromAPI[i].minParticipants;
-            let maxParticipants = resultFromAPI[i].maxParticipants;
-            let image = resultFromAPI[i].image; 
-            let labels = resultFromAPI[i].labels;
-
-            let challenge_item = document.createElement("li");
-            challenge_item.classList.add('challenge-item');
-            
-            let setImage = document.createElement("img");
-            setImage.classList.add('challenge-image');
-            setImage.src = image;
-            challenge_item.append(setImage);
-
-            let setRoomImage = document.createElement("img");
-            setRoomImage.classList.add('room-image');
-                if (type == 'onsite') {
-                    setRoomImage.src = "static/iconOnline.svg";
-                } else {
-                    setRoomImage.src = "static/iconOnsite.svg";
-                }
-            challenge_item.append(setRoomImage);
-
-            let setRating = document.createElement("ul");
-            setRating.classList.add('rating');
-
-            /* I måpn av tid på slutet, fixa loop */
-            let setRating_star_1 = document.createElement("li");
-            setRating_star_1.classList.add('rating-star');
-            setRating.appendChild(setRating_star_1);
-
-            let setRating_star_2 = document.createElement("li");
-            setRating_star_2.classList.add('rating-star');
-            setRating.appendChild(setRating_star_2);
-
-            let setRating_star_3 = document.createElement("li");
-            setRating_star_3.classList.add('rating-star');
-            setRating.appendChild(setRating_star_3);
-
-            let setRating_star_4 = document.createElement("li");
-            setRating_star_4.classList.add('rating-star');
-            setRating.appendChild(setRating_star_4);
-
-            let setRating_star_5 = document.createElement("li");
-            setRating_star_5.classList.add('rating-star');
-            setRating.appendChild(setRating_star_5);
-            
-            if (rating < 1) {
-                setRating_star_1.classList.add('rating-star');
-            } else if (rating == 1 || rating < 2) {
-                setRating_star_1.classList.add('active');
-            } else if (rating == 2  || rating < 3) {
-                setRating_star_1.classList.add('active');
-                setRating_star_2.classList.add('active');    
-            } else if (rating == 3  || rating < 4) {
-                setRating_star_1.classList.add('active');
-                setRating_star_2.classList.add('active');
-                setRating_star_3.classList.add('active');
-            } else if (rating == 4  || rating < 5) {
-                setRating_star_1.classList.add('active');
-                setRating_star_2.classList.add('active');
-                setRating_star_3.classList.add('active');
-                setRating_star_4.classList.add('active');
-            } else {
-                setRating_star_1.classList.add('active');
-                setRating_star_2.classList.add('active');
-                setRating_star_3.classList.add('active');
-                setRating_star_4.classList.add('active');
-                setRating_star_5.classList.add('active'); 
-            }
-            
-            challenge_item.appendChild(setRating);
-            
-            let setTitle = document.createElement("h3");
-            setTitle.classList.add('challenge-title');
-            setTitle.textContent = title;
-            challenge_item.appendChild(setTitle);
-
-            let setParticipants = document.createElement("small");
-            setParticipants.classList.add('challenge-meta');
-            setParticipants.innerHTML = `${minParticipants} - ${maxParticipants} participants`;
-            challenge_item.appendChild(setParticipants);
-
-            let setDescription = document.createElement("p");
-            setDescription.classList.add('challenge-description');
-            setDescription.textContent = description;
-            challenge_item.appendChild(setDescription);
-
-            let btnBook = document.createElement("button");
-            btnBook.classList.add('btnBook');
-
-            btnBook.setAttribute("data-id", id); 
-                
-                if (type == 'onsite') {
-                    btnBook.textContent = 'Take challenge online';
-                } else {
-                    btnBook.textContent = 'Book this room';
-                }
-            challenge_item.appendChild(btnBook);
-
-            btnBook.addEventListener("click", function(){
-                console.log(id);
-                // referens till bokningsfunktion
-            });
-
-    
-            challenge_list.appendChild(challenge_item);
-            
-            console.log(title);
-      
-    };
-
-
-
-}  
+    showAllChallenges(resultFromAPI, challenge_list);
+};
--- 02-esc/index.js
+++ 02-esc/index.js
@@ -1,5 +1,6 @@
 /* import { info } from "sass";  */
 import { getDataFromAPI } from "./getDataFromAPI.js";
+import { showAllChallenges } from "./showAllChallenges.js";
 
 const challenge_list = document.querySelector('.challenge-list');
 
@@ -14,264 +15,24 @@ const challenge_list = document.querySelector('.challenge-list');
 
  let resFromAPI = []; 
  let ratings;
-
-
-// First get all API data from getDataFromAPI
+ let sortRating = [];
+ let output = [];
 
 resFromAPI = await getDataFromAPI();
 
- //console.log(getDataFromAPI.rating);
+function sortRatingArray(resFromAPI){ 
 
- //ratings = resFromAPI.rating;
-
- //console.log(resFromAPI[0].rating);
-
-
- /* control function */
-
-function checkRatingIndex(resFromAPI){
-
-  let r = [];
-
-  for(let i = 0; i < resFromAPI.length; i++){
-
-    r = resFromAPI[i].rating;
-
-
-    if(r === 4.5){
-      return r;
-    }
-  }
-}
-
-console.log(resFromAPI.findIndex(checkRatingIndex));
- 
-
-
- 
-
-
-
-
-
-// Then select the three highest rating-star with an if statements
-
-  function getThreeHighestRatingStar(resFromAPI){
-
-    // local variables
-
-    let highest = -Infinity;
-    let secondHighest = -Infinity;
-    let thirdHighest = -Infinity;
-    let r;
-    
-    
-
-    // get the first highest rating-star
-
-    for(let n = 0; n < resFromAPI.length; n++){
-
-       r = resFromAPI[n].rating;
-       
-
-      highest = Math.max(highest, r);
-      
-    }
-    console.log(highest);
-    
-
-
-    // get the second-highest rating-star  
   
-  
-    for(let n = 0; n < resFromAPI.length; n++){
-
-      r = resFromAPI[n].rating;
-      
-
-      if(r < highest){        
-  
-        secondHighest = Math.max(secondHighest, r);
-        
-      }
-    }
-    console.log(secondHighest);
-
-
-    // get the third highest rating-star  
-  
-  
-    for(let n = 0; n < resFromAPI.length; n++){
-
-      r = resFromAPI[n].rating;
-
-      if(r < secondHighest){
-  
-        thirdHighest = Math.max(thirdHighest, r);
-      }
-    }
-    console.log(thirdHighest);
-
-
-  /************** creating of card ***************/
-    
-  
-
-  for(let i = 0; i < resFromAPI.length; i++){
-
-    //ratings = resFromAPI[i].rating;
-
-    let id = resFromAPI[i].id;
-    let type = resFromAPI[i].type;
-    let title = resFromAPI[i].title;
-    let description = resFromAPI[i].description;
-    let minParticipants = resFromAPI[i].mindescription;
-    let maxParticipants = resFromAPI[i].maxParticipants;
-    let image = resFromAPI[i].image;
-
-    
-    
-
-    // create dynamic li-element
-
-    let challenge_item = document.createElement('li');
-    challenge_item.classList.add('challenge-item');
-
-    let setImage = document.createElement('img');
-    setImage.classList.add('challenge-image');
-    setImage.src = image;
-    challenge_item.append(setImage);
-
-    let setRoomImage = document.createElement('image');
-    setRoomImage.classList.add('room-image');
-      if(type == 'onsite')
-      {
-        setRoomImage.src = "static/iconOnline.svg"
-      }
-      else
-      {
-        setRoomImage.src = "static/iconicOnsite.svg";
-      }
-
-    challenge_item.append(setRoomImage);
-
-
-    // create dynamic ul 
-
-    let setRatingUl = document.createElement('ul');
-    setRatingUl.classList.add('rating');
-
-    // create dynamic li elements
-
-    let setRatingStar1 = document.createElement('li');
-    setRatingStar1.classList.add('rating-star');
-    setRatingUl.appendChild(setRatingStar1);
-
-    let setRatingStar2 = document.createElement('li');
-    setRatingStar2.classList.add('rating-star');
-    setRatingUl.appendChild(setRatingStar2);
-
-    let setRatingStar3 = document.createElement('li');
-    setRatingStar3.classList.add('rating-star');
-    setRatingUl.appendChild(setRatingStar3);
-
-    let setRatingStar4 = document.createElement('li');
-    setRatingStar4.classList.add('rating-star');
-    setRatingUl.appendChild(setRatingStar4);
-
-    let setRatingStar5 = document.createElement('li');
-    setRatingStar5.classList.add('rating-star');
-    setRatingUl.appendChild(setRatingStar5);
-
-    if(r == highest){
-
-      // print out between 2 < 3
-      setRatingStar1.classList.add('active');
-      setRatingStar2.classList.add('active');
-      setRatingStar3.classList.add('active');
-      setRatingStar4.classList.add('active');
-    }
-
-
-    else if(r < secondHighest){
-
-      // Print out between 3 < 4
-      setRatingStar1.classList.add('active');
-      setRatingStar2.classList.add('active');
-      setRatingStar3.classList.add('active');
-      setRatingStar4.classList.add('active');
-      
-    }
-    else if(r < thirdHighest){
-
-      // print out between 4 < 5
-      setRatingStar1.classList.add('active');
-      setRatingStar2.classList.add('active');
-      setRatingStar3.classList.add('active');
-      
-    }
-    
-
-   
-
-    challenge_item.appendChild(setRatingUl);
-
-    let set_Title = document.createElement('h3');
-    set_Title.classList.add('challenge-title');
-    set_Title.textContent = title;
-    challenge_item.appendChild(set_Title);
-
-    let set_Participants = document.createElement('small');
-    set_Participants.classList.add('challenge-meta');
-    set_Participants.innerHTML = `${minParticipants} - ${maxParticipants} participants`;
-    challenge_item.appendChild(set_Participants);
-
-    let set_Description = document.createElement('p');
-    set_Description.classList.add('challenge-description');
-    set_Description.textContent = description;
-    challenge_item.appendChild(set_Description);
-
-    let btn_Book = document.createElement('button');
-    btn_Book.classList.add('btnBook');
-    btn_Book.setAttribute('data-id', id);
-
-      if(type == 'onsite')
-      {
-        btn_Book.textContent = 'Take challenge online';
-      }else
-      {
-        btn_Book.textContent = 'Book this room';
-      }
-
-    challenge_item.appendChild(btn_Book);
-
-    btn_Book.addEventListener('click', function(){
-
-      // referens till bokningsfunktionen
+  resFromAPI.sort(function(a, b){
+      return b.rating - a.rating
     });
 
-    challenge_list.appendChild(challenge_item);
-
-
-
-   
-  }
-
+    
+    let newArray = [];
+    newArray.push(resFromAPI[0], resFromAPI[1], resFromAPI[2]);
+    
   
+  showAllChallenges(newArray, challenge_list);
+}
 
-
-}  
-
-// execute function
-
-getThreeHighestRatingStar(resFromAPI);
-
-
-
-
-
-
-
-
-
-
+sortRatingArray(resFromAPI);
